408d07e89205a5a43b3bd3ce25198c05fed44ee6
sahil5910 <109588023+sahil5910@users.noreply.github.com>
2023-09-28 16:46:15 +0530

    Add files via upload

---
+++ src/sections/Dashboard/Friends.js
@@ -0,0 +1,119 @@
+import React, { useEffect } from "react";
+import { Dialog, DialogContent, Slide, Stack, Tab, Tabs } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  FetchFriendRequests,
+  FetchFriends,
+  FetchUsers,
+} from "../../redux/slices/app";
+import { FriendElement, FriendRequestElement, UserElement } from "../../components/UserElement";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const UsersList = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(FetchUsers());
+  }, []);
+
+  const { users } = useSelector((state) => state.app);
+
+  return (
+    <>
+      {users.map((el, idx) => {
+        return <UserElement key={el._id} {...el} />;
+      })}
+    </>
+  );
+};
+
+const FriendsList = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(FetchFriends());
+  }, []);
+
+  const { friends } = useSelector((state) => state.app);
+
+  return (
+    <>
+      {friends.map((el, idx) => {
+        return <FriendElement key={el._id} {...el} />;
+      })}
+    </>
+  );
+};
+
+const RequestsList = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(FetchFriendRequests());
+  }, []);
+
+  const { friendRequests } = useSelector((state) => state.app);
+
+  return (
+    <>
+      {friendRequests.map((el, idx) => {
+        return <FriendRequestElement key={idx} {...el.sender} id={el._id} />;
+      })}
+    </>
+  );
+};
+
+const Friends = ({ open, handleClose }) => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Dialog
+      fullWidth
+      maxWidth="xs"
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+      sx={{ p: 4 }}
+    >
+      {/* <DialogTitle>{"Friends"}</DialogTitle> */}
+      <Stack p={2} sx={{ width: "100%" }}>
+        <Tabs value={value} onChange={handleChange} centered>
+          <Tab label="Explore" />
+          <Tab label="Friends" />
+          <Tab label="Requests" />
+        </Tabs>
+      </Stack>
+      <DialogContent>
+        <Stack sx={{ height: "100%" }}>
+          <Stack spacing={2.4}>
+            {(() => {
+              switch (value) {
+                case 0: // display all users in this list
+                  return <UsersList />;
+
+                case 1: // display friends in this list
+                  return <FriendsList />;
+
+                case 2: // display request in this list
+                  return <RequestsList />;
+
+                default:
+                  break;
+              }
+            })()}
+          </Stack>
+        </Stack>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default Friends;
